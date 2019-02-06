1827188d164986028fc21da7d67e2aa87b995cad
Diogo Gomes Viana <dgomesvi@everis.com>
2019-02-06 14:02:58 -0200

    Enviando script para o repositório!

---
+++ ScriptPython.py
@@ -0,0 +1,37 @@
+import csv, os
+
+reportFilename = "C:\Users\dgomesvi\Desktop\Diogo Viana"
+reportDelimiter = ';'
+outputPath = '.'
+
+def main():
+    splitFile(reportFilename)
+
+def splitFile(filename):
+    with open(filename, "r", newline='', encoding="utf8") as originalFile:
+        reader = csv.reader(originalFile, delimiter=reportDelimiter)
+        current_piece = 1
+        outputFile = os.path.join(outputPath, str(current_piece) + filename)
+        with open(outputFile, "w", newline='', encoding="utf8") as outputFileOpen:
+            current_out_writer = csv.writer(outputFileOpen, delimiter=reportDelimiter)
+            header = next(reader)
+            current_out_writer.writerow(header)
+        outputFileOpen.close()
+        for row in enumerate(reader):
+            if os.path.getsize(outputFile) > 2000:
+                current_piece += 1
+                outputFile = os.path.join(outputPath, str(current_piece) + filename)
+                with open(outputFile, "a", newline='', encoding="utf8") as outputFileOpen:
+                    current_out_writer = csv.writer(outputFileOpen, delimiter=reportDelimiter)
+                    current_out_writer.writerow(header)
+                    current_out_writer.writerow(row)
+                    outputFileOpen.close()
+            else:
+                with open(outputFile, "a", newline='', encoding="utf8") as outputFileOpen:
+                    current_out_writer = csv.writer(outputFileOpen, delimiter=reportDelimiter)
+                    current_out_writer.writerow(row)
+                    outputFileOpen.close()
+
+if __name__ == '__main__':
+    main()
+            
